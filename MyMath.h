46db3f73745cc4c6b9f5505602ebbe70edf46404
Tanahara Koa <k024g0007@g.neec.ac.jp>
2025-02-24 01:57:48 +0900

    三角形をレンダリングパイプラインを通し描画 背面カリング済み

---
+++ MyMath.h
@@ -0,0 +1,108 @@
+﻿#pragma once
+
+static const int kRowHeight = 20;
+static const int kColumnWidth = 60;
+
+struct Vector3 {
+    float x, y, z;
+
+    // Vector3 operator+(const Vector3& other) { return { x + other.x, y + other.y , z + other.z}; }
+    // Vector3 operator-(const Vector3& other) { return { x - other.x, y - other.y, z - other.z }; }
+    // Vector3 operator*(const Vector3& other) { return { x * other.x, y * other.y, z * other.z }; }
+    // Vector3 operator/(const Vector3& other) { return { x / other.x, y / other.y , z / other.z}; }
+    // Vector3 operator=(const Vector3& other) { return { x = other.x, y = other.y , z = other.z}; }
+    // Vector3 operator+=(const Vector3& other) { return { x += other.x, y += other.y, z += other.z }; }
+    // Vector3 operator-=(const Vector3& other) { return { x -= other.x, y -= other.y, z -= other.z }; }
+    // Vector3 operator*=(const Vector3& other) { return { x *= other.x, y *= other.y, z *= other.z}; }
+    // Vector3 operator/=(const Vector3& other) { return { x /= other.x, y /= other.y, z /= other.z }; }
+};
+
+struct Matrix4x4 {
+
+    float m[4][4];
+};
+
+// 加算
+Vector3 Add(const Vector3& v1, const Vector3& v2);
+
+// 減算
+Vector3 Subtract(const Vector3& v1, const Vector3& v2);
+
+// スカラー倍
+Vector3 Multiply(float scalar, const Vector3& v);
+
+// 内積
+float Dot(const Vector3& v1, const Vector3& v2);
+
+// 長さ
+float Length(const Vector3& v);
+
+// 正規化
+Vector3 Normalize(const Vector3& v);
+
+//クロス積
+Vector3 Cross(const Vector3& v1, const Vector3& v2);
+
+//================================================
+// 4x4行列関数
+//================================================
+
+// 行列の加法
+Matrix4x4 Add(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 行列の減法
+Matrix4x4 Subtract(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 行列の積
+Matrix4x4 Multiply(const Matrix4x4& m1, const Matrix4x4& m2);
+
+// 逆行列
+Matrix4x4 Inverse(const Matrix4x4& m);
+
+// 転置行列
+Matrix4x4 Transpose(const Matrix4x4& m);
+
+// 単位行列
+Matrix4x4 MakeIdentity4x4();
+
+// 平行移動行列
+Matrix4x4 MakeTranslateMatrix(const Vector3& translate);
+
+// 拡大縮小行列
+Matrix4x4 MakeScaleMatrix(const Vector3& scale);
+
+// 　座標変換
+Vector3 TransformCoord(const Vector3& vector, const Matrix4x4& matrix);
+
+// x軸回転行列
+Matrix4x4 MakeRotationXMatrix(float radian);
+
+// y軸回転行列
+Matrix4x4 MakeRotationYMatrix(float radian);
+
+// z軸回転行列
+Matrix4x4 MakeRotationZMatrix(float radian);
+
+// 3次元アフィン変換
+Matrix4x4 makeAffineMatrix(const Vector3& scale, const Vector3& rotate, const Vector3& translate);
+
+//================================================
+// 　レンダリングパイプライン用
+//================================================
+
+// 透視投影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip);
+
+//正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip);
+
+//ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth);
+
+//================================================
+// 　値確認用
+//================================================
+
+void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix);
+
+void VectorScreenPrintf(int x, int y, const Vector3& vector);
--- main.cpp
+++ main.cpp
@@ -1,65 +1,140 @@
+#include "MyMath.h"
 #include <Novice.h>
+#include <cstdint>
 
 const char kWindowTitle[] = "LC1A_16_タナハラ_コア_タイトル";
 
-struct Vector2 {
-	float x;
-	float y;
-};
-
 //==============================
-//関数定義
+// 関数定義
 //==============================
 
 // Windowsアプリでのエントリーポイント(main関数)
-int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
+int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int)
+{
 
-	// ライブラリの初期化
-	Novice::Initialize(kWindowTitle, 1280, 720);
+    // ライブラリの初期化
+    Novice::Initialize(kWindowTitle, 1280, 720);
 
-	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+    // キー入力結果を受け取る箱
+    char keys[256] = { 0 };
+    char preKeys[256] = { 0 };
 
-	//==============================
-	// 変数初期化
-	//==============================
+    //==============================
+    // 変数初期化
+    //==============================
 
-	// ウィンドウの×ボタンが押されるまでループ
-	while (Novice::ProcessMessage() == 0) {
-		// フレームの開始
-		Novice::BeginFrame();
+    // クロス積確認用
+    /*Vector3 v1 = { 1.2f, -3.9f, 2.5f };
+    Vector3 v2 = { 2.8f, 0.4f, -1.3f };
 
-		// キー入力を受け取る
-		memcpy(preKeys, keys, 256);
-		Novice::GetHitKeyStateAll(keys);
+    Vector3 cross = Cross(v1, v2);*/
 
-		///
-		/// ↓更新処理ここから
-		///
+    Vector3 rotate = { 0.0f, 0.0f, 0.0f };
+    Vector3 translate = { 0.0f, 0.0f, 5.0f };
 
-		///
-		/// ↑更新処理ここまで
-		///
+    Vector3 cameraPos = { 0.0f, 0.0f, -5.0f };
 
-		///
-		/// ↓描画処理ここから
-		///
+    // ウィンドウの×ボタンが押されるまでループ
+    while (Novice::ProcessMessage() == 0) {
+        // フレームの開始
+        Novice::BeginFrame();
 
-		///
-		/// ↑描画処理ここまで
-		///
+        // キー入力を受け取る
+        memcpy(preKeys, keys, 256);
+        Novice::GetHitKeyStateAll(keys);
 
-		// フレームの終了
-		Novice::EndFrame();
+        ///
+        /// ↓更新処理ここから
+        ///
 
-		// ESCキーが押されたらループを抜ける
-		if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
-			break;
-		}
-	}
+        // 前後に三角形を動かす
+        if (keys[DIK_W]) {
+            translate.z += 0.05f;
+        }
 
-	// ライブラリの終了
-	Novice::Finalize();
-	return 0;
+        if (keys[DIK_S]) {
+            translate.z -= 0.05f;
+        }
+
+        // 左右に三角形を動かす
+        if (keys[DIK_A]) {
+            translate.x -= 0.05f;
+        }
+
+        if (keys[DIK_D]) {
+            translate.x += 0.05f;
+        }
+
+        // y軸回転
+        rotate.y += 0.05f;
+
+        // 背面カリング時rotate.y = 0でも表示されるかの確認用
+        //rotate.y = 0.0f;
+
+        Matrix4x4 worldMatrix = makeAffineMatrix({ 1.0, 1.0f, 1.0f }, rotate, translate);
+        Matrix4x4 cameraMatrix = makeAffineMatrix({ 1.0, 1.0f, 1.0f }, { 0.0f, 0.0f, 0.0f }, cameraPos);
+        Matrix4x4 viewMatrix = Inverse(cameraMatrix);
+        Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(1280) / float(720), 0.1f, 100.0f);
+        Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
+        Matrix4x4 viewPortMatrix = MakeViewportMatrix(0.0f, 0.0f, 1280.0f, 720.0f, 0.0f, 1.0f);
+
+        Vector3 kLocalVertices[3] = {
+            { 0.0f, 1.0f, 0.0f },
+            { 1.0f, -1.0f, 0.0f },
+            { -1.0f, -1.0f, 0.0f }
+        };
+
+        Vector3 screenVertices[3];
+
+        for (uint32_t i = 0; i < 3; ++i) {
+            Vector3 ndcVertex = TransformCoord(kLocalVertices[i], worldViewProjectionMatrix);
+            screenVertices[i] = TransformCoord(ndcVertex, viewPortMatrix);
+        }
+        ///
+        /// ↑更新処理ここまで
+        ///
+
+        ///
+        /// ↓描画処理ここから
+        ///
+
+        // クロス積確認用
+        /* VectorScreenPrintf(0, 0, cross);*/
+
+        // 背面カリング
+        Vector3 vec1 = { screenVertices[1].x - screenVertices[0].x,
+            screenVertices[1].y - screenVertices[0].y,
+            0.0f };
+
+        Vector3 vec2 = { screenVertices[2].x - screenVertices[0].x,
+            screenVertices[2].y - screenVertices[0].y,
+            0.0f };
+
+       Vector3 cross =  Cross(vec1, vec2);
+
+        // 三角形を描画(背面カリングを考慮)
+        if (cross.z > 0) {
+            Novice::DrawTriangle(
+                int(screenVertices[0].x), int(screenVertices[0].y),
+                int(screenVertices[1].x), int(screenVertices[1].y),
+                int(screenVertices[2].x), int(screenVertices[2].y),
+                RED, kFillModeSolid);
+        }
+
+        ///
+        /// ↑描画処理ここまで
+        ///
+
+        // フレームの終了
+        Novice::EndFrame();
+
+        // ESCキーが押されたらループを抜ける
+        if (preKeys[DIK_ESCAPE] == 0 && keys[DIK_ESCAPE] != 0) {
+            break;
+        }
+    }
+
+    // ライブラリの終了
+    Novice::Finalize();
+    return 0;
 }
